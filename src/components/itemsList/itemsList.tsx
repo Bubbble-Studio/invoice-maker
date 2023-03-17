c95a9cd7d7b2fb67f555ee86a9a33f65ec30ab29
ashwath462 <ashwatharora462@gmail.com>
2023-03-17 18:27:03 +0530

    styles

--- src/components/itemsList/itemsList.tsx
+++ src/components/itemsList/itemsList.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-
+import {
+    useColorMode,
+    Switch,
+    Flex,
+    Button,
+    IconButton,
+} from "@chakra-ui/react";
+import styles from "./itemsList.module.css"
 const itemsList = ()=>{
 
     const [items, setItems] = useState<any>([{ name: "", quantity: 0, price: 0, total: 0 }]);
@@ -24,18 +31,39 @@ const itemsList = ()=>{
 
 
     return(
-        <div className="main__container">
-            <div className="invoice__items">
+        <div className= {styles.main__container} style={styles}>
+            <div className="invoice__items" >
                 <h3>Item List</h3>
+                <div className="items">
+                    <div className={styles.item_head} style={{display:"flex"}}>
+                        <div style={{padding:"1vw", margin:"1vw",width:"6%"}}>
+                            <p>Sr No.</p>
+                        </div>
+                        <div style={{padding:"1vw", margin:"1vw",width:"29%"}}>
+                            <p>Item Name</p>
+                        </div>
+
+                        <div style={{padding:"1vw", margin:"1vw", width:"15%"}}>
+                            <p>Qty</p>
+                        </div>
+
+                        <div style={{padding:"1vw", margin:"1vw", width:"15%"}}>
+                            <p>Price</p>
+                        </div>
+                        <div style={{padding:"1vw", margin:"1vw", width:"15%"}}>
+                            <p>Total Amount</p>
+                        </div>   
+                    </div>
+                </div>
                 {items?.map((item:any, i:number) => (
                 <div className="items" key={i}>
-                    <div className="item_i">
-                        <div>
-                            <p>S No.</p>
+                    <div className={styles.item_i} style={{display:"flex"}}>
+                        <div style={{padding:"1vw", margin:"1vw",width:"6%"}}>
+                            {/* <p>Sr No.</p> */}
                             <h4>{i+1}</h4>
                         </div>
-                        <div>
-                            <p>Item Name</p>
+                        <div style={{padding:"1vw", margin:"1vw",width:"29%"}}>
+                            {/* <p>Item Name</p> */}
                             <input
                             type="text"
                             name="name"
@@ -43,8 +71,8 @@ const itemsList = ()=>{
                             />
                         </div>
 
-                        <div>
-                            <p>Qty</p>
+                        <div style={{padding:"1vw", margin:"1vw", width:"15%"}}>
+                            {/* <p>Qty</p> */}
                             <input
                             type="number"
                             name="quantity"
@@ -52,30 +80,37 @@ const itemsList = ()=>{
                             />
                         </div>
 
-                        <div>
-                            <p>Price</p>
+                        <div style={{padding:"1vw", margin:"1vw", width:"15%"}}>
+                            {/* <p>Price</p> */}
                             <input
                             type="number"
                             name="price"
                             onChange={(e) => handlerChange(e, i)}
                             />
                         </div>
-                        <div>
-                            <p>Total Amount</p>
+                        <div style={{padding:"1vw", margin:"1vw", width:"15%"}}>
+                            {/* <p>Total Amount</p> */}
                             <h4>{item.total}</h4>
                         </div>
 
-                        <button className="delete-btn" onClick={() => deleteItem(i)}>
-                            Delete
-                        </button>
+                        <div style={{padding:"1vw", margin:"1vw"}}>
+                            <Button className="delete-btn" onClick={() => deleteItem(i)}>
+                                Delete
+                            </Button>
+                        </div>    
                     </div>
                 </div>
                 ))}
             </div>
+            <div style={{display: "flex", justifyContent:"center", alignItems:"center"}}>
+                <Button className="add_item-btn" onClick={addItem}>
+                    Add New Item
+                </Button>
+            </div>
 
-            <button className="add_item-btn" onClick={addItem}>
-                Add New Item
-            </button>
+            <div className="totalSum" style={{textAlign:"end", margin:"1vw"}}>
+                Total : {totalAmount}
+            </div>
         </div>
     );
 }
