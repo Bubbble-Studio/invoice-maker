3bc35b6e66d63d17478f054708978441eea9f738
Arinjay Saraf <arinjaysaraf@gmail.com>
2023-06-30 22:32:39 +0530

    navbar is responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import InvoiceArea from "../components/InvoiceArea/InvoiceArea";
 import { Heading, Text, Box, Center, Flex, Divider } from "@chakra-ui/react";
+import styles from "./index.module.scss";
 
 export default function Home() {
   return (
@@ -15,41 +15,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Flex
-          alignItems={"center"}
-          justifyContent={"center"}
-          flexDirection={"column"}
-          bgGradient={
-            "linear(107.21deg, #BAE4F5 0%, #CDF1BF 53.31%, #F8FAA8 113.36%)"
-          }
-        >
-          <Navbar />
-          <Flex flexDirection={"column"} textAlign={"center"} color={"black"}>
-            <h3
-              style={{
-                fontFamily: "Poppins",
-                fontSize: "42px",
-                fontWeight: "700",
-                color: "#333333",
-                marginTop: "0.8rem",
-              }}
-            >
-              Invoice Maker
-            </h3>
-            <p
-              style={{
-                fontFamily: "Poppins",
-                fontSize: "18px",
-                color: "#333333",
-                fontWeight: "400",
-                marginBottom: "2rem",
-              }}
-            >
+        <div className={styles.headerContainer}>
+          <div className={styles.navbar}>
+            <Navbar />
+            <div className={styles.headerTitle}>
+              <h3>Invoice Maker</h3>
+            </div>
+          </div>
+
+          <div className={styles.headerContent}>
+            <p>
               Simplify your billing process with our user-friendly invoice
               maker, making financial management a breeze
             </p>
-          </Flex>
-        </Flex>
+          </div>
+        </div>
         <Divider />
         <InvoiceArea />
         <Divider />
